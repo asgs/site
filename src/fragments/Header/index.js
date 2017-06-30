f5563573bfe8d321188fb67f13c0864eda79df1f
A S Gowri Sankar <asgosa.eeegct@gmail.com>
2017-07-01 01:24:26 +0530

    header changes
    1. Remove documentation and put it under Framework li.
    2. Shorten the link names of items.
    3. Introduce a Github source link under Framework li. (Maybe remove the separate Github link on the right side?)

--- src/fragments/Header/index.js
+++ src/fragments/Header/index.js
@@ -64,28 +64,28 @@ export default class Header extends Component {
             <nav className={`${styles.navRight} ${mobileNav}`}>
               <ul className={styles.navItems}>
                 <li className={styles.navItem}>
-                  <Link to='/framework/docs' className={styles.link}>
-                    Documentation
-                  </Link>
-                </li>
-                <li className={styles.navItem}>
                   <Link to='/framework' className={styles.link}>
                     Framework <svg className={styles.caret} width='8' height='4' viewBox='62 7 10 6'><path d='M71.884 7.698l-4.56 5.116c-.013.022-.008.05-.026.07-.083.084-.192.12-.3.116-.106.004-.214-.033-.295-.117-.02-.02-.014-.047-.028-.068L62.115 7.7c-.154-.16-.154-.42 0-.58.156-.16.408-.16.563 0L67 11.97l4.322-4.85c.155-.16.406-.16.56 0 .157.16.157.418.002.578z' fill='#fff' /></svg>
                   </Link>
                   <ul className={styles.subNavItems}>
                     <li>
                       <Link to='/framework' className={`${styles.link} ${styles.noMobile}`}>
-                        About Serverless
+                        About
                       </Link>
                     </li>
                     <li>
                       <Link to='/framework/docs' className={styles.link}>
-                        Framework docs
+                        Documentation
                       </Link>
                     </li>
                     <li>
+                      <a href='https://www.github.com/serverless/serverless' target='_blank' rel='noopener noreferrer' className={styles.link}>
+                        Source
+                      </a>
+                    </li>
+                    <li>
                       <Link to='/framework/status' className={styles.link}>
-                        Project Status
+                        Status
                       </Link>
                     </li>
                   </ul>
@@ -97,17 +97,17 @@ export default class Header extends Component {
                   <ul className={styles.subNavItems}>
                     <li>
                       <a href='https://gitter.im/serverless/serverless' target='_blank' rel='noopener noreferrer' className={styles.link}>
-                        Chat on Gitter
+                        Gitter
                       </a>
                     </li>
                     <li>
                       <a href='http://forum.serverless.com/' target='_blank' rel='noopener noreferrer' className={styles.link}>
-                        Discuss on the Forums
+                        Forums
                       </a>
                     </li>
                     <li>
                       <Link to='/community/meetups' className={`${styles.link} ${styles.noMobile}`}>
-                        Join a Meetup
+                        Meetups
                       </Link>
                     </li>
                     <li>
@@ -129,12 +129,12 @@ export default class Header extends Component {
                   <ul className={styles.subNavItems}>
                     <li>
                       <Link to='/company/team' className={styles.link}>
-                        Meet the team
+                        Team
                       </Link>
                     </li>
                     <li>
                       <Link to='/company/jobs' className={styles.link}>
-                        We are hiring
+                        Jobs
                       </Link>
                     </li>
                     {/* <li>
